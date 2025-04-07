d9a9b95a5c7a51aa49b57b96684924cf704a1acd
Andrés López <andres.lopez-pardo@t-systems.com>
2025-04-07 14:26:27 +0200

    Adding count while.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,26 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Welcome to demo Project!");
-        count(1,100);
+        System.out.printf("Welcome to demo Project!\n");
+        countFor(1,10);
+        countWithWhile(1,10);
     }
 
-    private static void count(int ini, int end) {
-        for (int i = ini; i <= end; i++) {
-            System.out.println("i = " + i);
+    private static void countWithWhile(int ini, int end) {
+        System.out.println("\ncountWithWhile ==================");
+        while(ini<=end){
+            System.out.println("while i = " + ini);
+            ++ini;
         }
     }
 
+    private static void countFor(int ini, int end) {
+        System.out.println("\ncountFor =======================");
+        for (int i = ini; i <= end; i++) {
+            System.out.println("count i = " + i);
+        }
+    }
 
 }
